bc156a1a67fb912a5fd0fc0a470fab3da99555bd
Agnes IRADUKUNDA <101483232+kunda4@users.noreply.github.com>
2023-09-22 20:58:09 +0200

    react setup

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './App.css'
+import Navbar from './component/navabar'
+
+function App() {
+
+
+  return (
+    <>
+    <Navbar/>
+      <h1>Fun facts about React</h1>
+      <ul>
+        <li>Was first released in 2023</li>
+        <li> Was orginally created by Jordan Walker</li>
+        <li>Has well over 100k stars on GITHUB</li>
+        <li>Is mantained by Facebook</li>
+        <li>Powers thousands of enterprise app, including mobile apps</li>
+      </ul>
+    </>
+  )
+}
+
+export default App
---
+++ src/component/navabar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+export default function Navbar() {
+  return (
+    <nav>
+      <div className="navleft">
+        <img src="./" alt="" />
+        <h3>Hello Africa</h3>
+        
+      </div>
+    </nav>
+  );
+}
